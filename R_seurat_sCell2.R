e90ee220dd6968e559e5007fee6b1a453230a575
Nicolo Bellin <97742856+NicoloBellin@users.noreply.github.com>
2024-05-23 13:34:36 +0200

    Update R_seurat_sCell2.R

--- R_seurat_sCell2.R
+++ R_seurat_sCell2.R
@@ -43,7 +43,7 @@ ElbowPlot(df_sub)
 
 #Run the Algorithm of Clustering
 df_sub <- FindNeighbors(df_sub, reduction = "pca", dims = 1:50)
-df_sub <- FindClusters(df_sub, resolution = 0.8)
+df_sub <- FindClusters(df_sub, resolution = 0.3)
 
 #Run UMAP
 df_sub <- RunUMAP(df_sub, dims = 1:50)
@@ -51,38 +51,9 @@ df_sub <- RunUMAP(df_sub, dims = 1:50)
 #Plot 
 DimPlot(df_sub, reduction = "umap") +xlab("UMAP 1") + ylab("UMAP 2")
 
-#Find all marker genes
-df.markers <- FindAllMarkers(df_sub, only.pos = TRUE)
-
-
-#Dplyr check and manage the genes
-top10 <- df.markers %>%
-  group_by(cluster) %>%
-  dplyr::filter(avg_log2FC > 1) %>%
-  slice_head(n = 10) %>%
-  ungroup() 
-
-#Make an heatmap 
-DoHeatmap(df_sub, features = top10$gene) + NoLegend()
-
 #Wnta gene:Hcha.Hcha1001G7804 and cortex gene:Hcha.Hcha1500G10526 
 wnta <- FeaturePlot(df_sub, features = "gene:Hcha.Hcha1001G7804") + xlab('UMAP1') + ylab("UMAP2")
 cortex <- FeaturePlot(df_sub, features = "gene:Hcha.Hcha1500G10526") + xlab('UMAP1') + ylab("UMAP2")
 
 
-#Check the co-espressione 
-wnta_m <- subset(df_sub, features = 'gene:Hcha.Hcha1001G7804')
-cortex_m <- subset(df_sub, features = 'gene:Hcha.Hcha1500G10526')
 
-#Make a data frame
-df_coexp <- data.frame(WNTA=wnta_m@assays$RNA@layers$data, CORTEX=cortex_m@assays$RNA@layers$data)
-
-#Subset the cluster
-df_coexp <- df_coexp[df_sub@meta.data$RNA_snn_res.0.5 == 0,]
-
-
-#Plot the relation
-plot(df_coexp$WNTA, df_coexp$CORTEX, col='black', pch = 16)
-model <- lm(df_coexp$CORTEX ~ df_coexp$WNTA)
-summary(model)
-abline(model, col= 'red')
